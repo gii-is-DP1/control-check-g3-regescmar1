58da6fe0d03283faa04a1861c96cbc06a570c075
Regina <regescmar1@alum.us.es>
2022-01-14 17:12:04 +0100

    Control lab

--- src/main/java/org/springframework/samples/petclinic/recoveryroom/RecoveryRoomType.java
+++ src/main/java/org/springframework/samples/petclinic/recoveryroom/RecoveryRoomType.java
@@ -1,11 +1,28 @@
 package org.springframework.samples.petclinic.recoveryroom;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
+@Table(name="recovery_room_types")
 public class RecoveryRoomType {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="id")
     Integer id;
+	@NotEmpty
+	@Size(min=5,max=50)
+	@Column(name="name",unique=true)
     String name;
 }
